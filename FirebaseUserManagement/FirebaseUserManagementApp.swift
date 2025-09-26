3f7234a87d9bb86d75258802f74322e10f6df04d
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-09-26 10:11:32 +0200

    Initial Commit

---
+++ FirebaseUserManagement/FirebaseUserManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseUserManagementApp.swift
+//  FirebaseUserManagement
+//
+//  Created by Thibault Giraudon on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseUserManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
